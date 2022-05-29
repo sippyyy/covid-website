393ec348a7358948d08f59e71af65f16356c82ff
Nguyen Thuy <nguyenthuy030800@gmail.com>
2022-05-29 18:09:36 +0700

    update responsive,logo, js

--- index.js
+++ index.js
@@ -26,10 +26,38 @@ window.onscroll = function(){
         header.classList.remove('visible')
     }
 
-    if(scrollY > 1200 && scrollY < 2000){
+    if(scrollY > 800 && scrollY < 2000){
         map.classList.add('active')
     }else{
         map.classList.remove('active')
     }
 }
 
+const btns = document.querySelectorAll('.solution__show-more')
+const cards = document.querySelectorAll('.solution__cards')
+
+btns.forEach((btn,index)=>{
+    btn.onclick=function(){
+        const card = cards[index]
+        if(card.classList.contains('active')){
+            card.classList.remove('active')
+            btn.innerText="Show more!"
+        }else{
+            card.classList.add('active')
+            btn.innerText="Hide"
+        }
+    }
+})
+
+
+const openNavBarBtn =document.querySelector('.header__navbar-open')
+
+openNavBarBtn.onclick=function(){
+    if(openNavBarBtn.classList.contains('active') && (header.classList.contains('active')) ){
+        openNavBarBtn.classList.remove('active')
+        header.classList.remove('active')
+    }else{
+        openNavBarBtn.classList.add('active')
+        header.classList.add('active')
+    }
+}
